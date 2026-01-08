b1741c09845f0ea0db248dd692a2afc6202b8505
priyankatapkeer <priyanka.tapkeer2426@gmail.com>
2026-01-08 13:25:13 +0530

    BasicTypes

---
+++ practice/basicTypes.ts
@@ -0,0 +1,20 @@
+console.log("This is a basic TypeScript file.");
+
+// 1.string
+let str = "Hello,Typescript";
+console.log(str);
+
+// 2.number 
+let num = 10;
+console.log(num);
+
+// 3.boolean
+let bool = true;
+console.log(bool);
+
+// 4.array
+let arr = [1,2,3,4];
+console.log("number array :",arr);
+
+let  arr2 = ['shiv','krishna','ram'];
+console.log("string array :",arr2);
